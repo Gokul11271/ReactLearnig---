dc99bfa22ae76cf571e08d3a0e0f63d17654ccaa
Gokul11271 <144220646+Gokul11271@users.noreply.github.com>
2025-09-14 12:08:49 +0530

    Add styling to Search component and update fetch logic for improved data handling

--- vite-project/src/components/searchbar.jsx
+++ vite-project/src/components/searchbar.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import "./searchbar.css"
 
 const Search = () => {
     const[input,SetInput]=useState("")
@@ -6,24 +7,24 @@ const Search = () => {
     const [Loading, SetLoading] = useState(true)
     
     useEffect(() => {
-        fetch("https://dummyjson.com/recipes")
+        fetch(`https://dummyjson.com/recipes/search?q=${input}`)
           .then((response) => response.json())
-            .then((data) => {
-                SetData(data.recipes);
-                    SetLoading(false);
+          .then((data) => {
+            SetData(data.recipes);
+            SetLoading(false);
           })
           .catch((error) => {
             console.log("error", error);
           });
-    }, [])
+    }, [input])
     
     useEffect(()=>{ },[input])
     if(Loading)return<h1>Loading....</h1>
 
     return (
       <>
-        <h1>searchbar </h1>
-        <label>Search here : </label>
+            <h1 className='se'>searchbar </h1>
+        <label className='search'>Search here : </label>
         <input
           type="text"
           placeholder="search.."
@@ -33,7 +34,7 @@ const Search = () => {
           }}
         />
 
-        <div>
+        <div className='LD'>
           {data.map((d) => (
              <span key={d.id}>{d.name}</span>
 
