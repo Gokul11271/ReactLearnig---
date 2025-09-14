a7c399a64e1e00cc72ac0c3b80b63062159d019a
Gokul11271 <144220646+Gokul11271@users.noreply.github.com>
2025-09-14 11:03:53 +0530

    dvd

--- vite-project/src/components/searchbar.jsx
+++ vite-project/src/components/searchbar.jsx
@@ -1,29 +1,45 @@
-import react, { useState, useffect } from 'react';
+import react, { useState, useEffect, use } from 'react';
 
 const search = () => {
-
+    const[input,SetInput]=useState("")
     const [data, SetData] = useState([])
     const [Loading, SetLoading] = useState(true)
     
     useEffect(() => {
-        fetch("")
-            .then((response) => response.json())
-            .then((data) => { 
-
-
-            })
-            .catch((error) => { console.log("error", error) });
+        fetch("https://dummyjson.com/recipes")
+          .then((response) => response.json())
+            .then((data) => {
+                SetData(data),
+                SetLoading(false)
+          })
+          .catch((error) => {
+            console.log("error", error);
+          });
     }, [])
     
+    useEffect(()=>{ },[input])
     if(Loading)return<h1>Loading....</h1>
 
     return (
-        <>
-            <h1>searchbar</h1>
-            <label>Searchhere : </label>
-            <input type="text" placeholder="search.." />
-            
-        </>
-    )
+      <>
+        <h1>searchbar </h1>
+        <label>Search here : </label>
+        <input
+          type="text"
+          placeholder="search.."
+          value={input}
+          onChange={(e) => {
+            SetInput(e.target.value);
+          }}
+        />
+        <p>this is the {SetData} </p>
+
+        <div>
+          
+            {data.map((d) => <span key={d.id}>{d.title}</span>)}
+          
+        </div>
+      </>
+    );
 }
 export default search;
